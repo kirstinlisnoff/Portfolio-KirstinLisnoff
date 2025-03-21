36c3961c5904d956b86463c8ec2d6a070b259410
Kiki <kirstinlisnoff5@gmail.com>
2025-03-20 22:27:25 -0400

    added styling to header, added about photo

--- Unsolved/New-Project/src/pages/about.jsx
+++ Unsolved/New-Project/src/pages/about.jsx
@@ -4,6 +4,7 @@ function About() {
     return (
         <section>
             <h2>About Me</h2>
+            <img src="/avatar.jpg" alt="Kirstin Lisnoff" class="about-img"/>
             <p>hellooooo</p>
         </section>
     );
