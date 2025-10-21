1bc9a3ff39baa57a025140c46117eb037b50035c
Kiki <kirstinlisnoff5@gmail.com>
2025-10-21 16:24:39 -0400

    Added handler for submitting contact form

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,6 +1,29 @@
 import "./contact.css"
 
 function Contact() {
+    const [submitted, setSubmitted] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const formData = new formData(e.target);
+
+        fetch("/", {
+            method: "POST",
+            body: formData,
+        })
+        .then(() => setSubmitted(true))
+        .catch((error) => alert(error));
+        };
+        
+    if (submitted) {
+        return (
+            <section className="contact">
+                <h1>contact</h1>
+                <h2>Thank you for reaching out! I'll get back to you soon.</h2>
+            </section>
+        );
+    }
+    
     return (
         <section className="contact">
             <h1>contact</h1>
