fed2d2800a7af40ab94a30a7cc5632e2ce8b6e2a
Kiki <kirstinlisnoff5@gmail.com>
2025-11-04 15:11:15 -0500

    Added files for projects and created functionality for hover to video

--- src/components/project2.jsx
+++ src/components/project2.jsx
@@ -1,19 +1,37 @@
 import './project.css'
+import { useRef } from 'react';
 
- function ProjectLeft({ title, image, description, githubLink }) {
+ function ProjectLeft({ title, image, video, description, githubLink }) {
+  const videoRef = useRef(null);
+  
+    const handleMouseEnter = () => videoRef.current?.play();
+    const handleMouseLeave = () => videoRef.current?.pause();
+
     return (
-      <div className="project">
-        <img src={image} alt={title} className="project-img" />
+      <div className="project" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+       
+         <div className='project-media'>
+          <img src={image} alt={title} className="project-img" />
+          <video
+            ref={videoRef}
+            className='hover-video'
+            loop
+            muted
+            playsInline
+            preload='none'
+            >
+              <source src={video} type='video/mp4'/>
+            </video>
+         </div>
         <div className='project-left'>
-        <h3 className="project-title">{title}</h3>
-        <p className="description">{description}</p>
-        <div className="project-links">
-          {/* <a href={deployedLink} target="_blank" rel="noopener noreferrer" className="project-link">
-            View Deployed App
-          </a> */}
-          <a href={githubLink} target="_blank" rel="noopener noreferrer" className="project-link">
-            <span>View GitHub Repo</span>
-          </a>
+         
+            <h3 className="project-title">{title}</h3>
+            <p className="description">{description}</p>
+            <div className="project-links">
+              \
+              <a href={githubLink} target="_blank" rel="noopener noreferrer" className="project-link">
+                <span>View GitHub Repo</span>
+              </a>
           </div> 
           </div>
           
--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -12,7 +12,8 @@ function Portfolio() {
         <div className="flightplan">
         <Project 
           title="Flightplan Travel Planner"
-          image="flightplan.png"
+          image="/flightplanIMG.png"
+          video="/flightplan2x.mp4"
           description="An interactive front-end web application that allows users to create and update a task list in preparation for travel. It features a sleek and modern design, with a focus on user experience and ease of use. The application is built using HTML, CSS, and JavaScript, and is designed to be responsive and mobile-friendly."
           deployedLink="https://p1t1-akcj.github.io/ProjectOne/"
           githubLink="https://github.com/P1T1-AKCJ/ProjectOne.git"
@@ -21,7 +22,8 @@ function Portfolio() {
       <div className="forkintheroad">
         <ProjectLeft 
           title="Fork in the Road"
-          image="flightplan.png"
+          image="/fitrIMG.png"
+          video="/FITR2x.mp4"
           description="A sleek full-stack app designed to help make the often stressful task of deciding where to eat a breeze.
           It features a user-friendly interface where users can input their dining preferences and receive personalized restaurant recommendations based on their location and desired cuisine type."
           deployedLink="https://p1t1-akcj.github.io/ProjectOne/"
@@ -33,7 +35,8 @@ function Portfolio() {
           title="StudyQuest"
           className="studyquest"
           description="A full-stack web application where students can create flashcard decks and study them in an engaging way. It features a user-friendly interface to organize study plans, play a variation of games with their decks, and monitor their performance over time."
-          image="flightplan.png"
+          image="/StudyQuestIMG.png"
+          video="/StudyQuest2x.mp4"
           deployedLink="https://p1t1-akcj.github.io/ProjectOne/"
           githubLink="https://github.com/P1T1-AKCJ/ProjectOne.git"
       />
